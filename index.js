8ab8bad3de7ef216f7bce82a8694d5543ef323ed
Geetha-005 <chillageetha323@gmail.com>
2025-02-15 15:03:50 +0530

    init

---
+++ index.js
@@ -0,0 +1,98 @@
+//Initialization
+const express = require('express')
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+const app = express()
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get('/',(req,res) =>{
+    res.send("Hello from Node API")
+})
+
+//GET
+app.get('/api/products', async(req,res) => {
+    try{
+        const products = await Product.find({})
+        res.status(200).json(products)
+    } catch(error){
+        res.status(500).json({messaage: error.messaage});
+    }
+});
+
+app.get('/api/product/:id', async(req,res) => {
+    try{
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch(error){
+        res.status(500).json({messaage: error.messaage});
+    }
+});
+
+//POST
+app.post('/api/products', async(req,res) => {
+    try{
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
+    } catch(error){
+        res.status(500).json({messaage: error.messaage});
+    }
+});
+
+//PUT
+app.put('/api/product/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await Product.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
+        
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.status(200).json(product);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+//DELETE
+app.delete('/api/product/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await Product.findByIdAndDelete(id);
+        
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.status(200).json({messaage: "Product deleted successfully"});
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+//Database connection
+mongoose.connect('mongodb+srv://geetachilla4:FMI8HPojeGb9jykx@cluster0.f8h0l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => {
+    console.log('Connected to database!');
+    app.listen(5000,() => {
+        console.log('Server is running on port 5000');
+    })
+})
+.catch(() => {
+    console.log('connect failed');
+});
+
+
+
+
+//FMI8HPojeGb9jykx
